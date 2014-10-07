a3d2c389da587618499f4da8490615e37f9ddfc4
Bob Evans <bizob2828@yahoo.com>
2014-10-06 22:17:36 -0400

    changed structure of code to demonstrate ng-click directive

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-meetup-angular-app
+Getting Started with AngularJS Presentation
 ==================
+# Background
+This app demonstrates how easy it is to bootstrap with Yeoman and angular generators.
+
+The code samples leverage services, constants, controllers, views of AngularJS.
+
+Grunt has been set up to serve code, watch for changes and reload, a few sample unit tests with code coverage.
+
+## Installation
+
+```
+git clone git@github.com:bizob2828/meetup-angular-app.git
+cd meetup-angular-app
+npm install && bower install
+```
+
+## Start Webserver with LiveReload
+```
+npm serve
+```
+
+## Run tests
+```
+grunt test
+```
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,23 +9,28 @@
  */
 angular.module('angularMeetupApp')
   .controller('AboutCtrl', function ($scope, $http, lastFmCreds) {
-    var params = {
-        method: 'user.getrecenttracks',
-        api_key: lastFmCreds.apiKey,
-        limit: 12,
-        user: lastFmCreds.user,
-        format: 'json'
-    };
-    $http.get(lastFmCreds.url, { params: params })
-        .success(function (data) {
-            if(data.error) {
-              $scope.error = data.message;
-            } else {
-              $scope.songs = data.recenttracks.track;
-            }
-        })
-        .error(function (data, status) {
-            $scope.error = data || 'Req is hosed';
-        });
+    $scope.recentTracks = function() {
+      $scope.loading = true;
+      $scope.songs = {};
+      var params = {
+          method: 'user.getrecenttracks',
+          api_key: lastFmCreds.apiKey,
+          limit: 12,
+          user: lastFmCreds.user,
+          format: 'json'
+      };
+      $http.get(lastFmCreds.url, { params: params })
+          .success(function (data) {
+              if(data.error) {
+                $scope.error = data.message;
+              } else {
+                $scope.songs = data.recenttracks.track;
+              }
+              $scope.loading = false;
+          })
+          .error(function (data, status) {
+              $scope.error = data || 'Req is hosed';
+          });
+      };
 
   });
